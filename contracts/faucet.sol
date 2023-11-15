c64edbc0516ebe176b72332f2b0a7facc6d5d694
shiyu <zhangshiyu@hashkey.com>
2023-11-15 14:14:52 +0800

    remove import https

--- contracts/faucet.sol
+++ contracts/faucet.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Faucet {
     IERC20 public token;
