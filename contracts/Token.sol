7605a4e4547c79f55271844be5ca0790c52658c7
shiyu <zhangshiyu@hashkey.com>
2024-01-25 17:52:24 +0800

    update hardhat version

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract Token is ERC20, ERC20Permit, Ownable {
-    constructor(uint256 initialSupply, string memory name, string memory symbol) ERC20(name, symbol) Ownable() {
+    constructor(uint256 initialSupply, string memory name, string memory symbol) ERC20(name, symbol) ERC20Permit(name) Ownable(msg.sender) {
       _mint(msg.sender, initialSupply * 10 ** decimals());
     }
 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,11 @@
 const BigNumber = require('bignumber.js');
+const { ethers } = require("hardhat")
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
+
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -15,19 +17,17 @@ async function main() {
 
   // We get the contract to deploy
 
-  const name = "IMX";
+  const name = "APE";
 
-  const Token = await ethers.getContractFactory("Token18");
-  const token = await Token.deploy(99999999999999,name,name);
-
-  await token.deployed();
-  const Faucet = await ethers.getContractFactory("Faucet");
-  const faucet = await Faucet.deploy(token.address,token.decimals());
-
+  const token = await ethers.deployContract("Token18",[99999999999999,name,name]);
   console.log(`${name} deployed to: ${token.address}`);
+
+  const faucet = await ethers.deployContract("Faucet",[token.address,token.decimals()]);
   console.log(`${name}'s Faucet deployed to: ${faucet.address}`);
+
   const amount = new BigNumber(9999999999999).times(new BigNumber(10).pow(await token.decimals()));
   await token.transfer(faucet.address,amount.toFixed());
+  console.log(`transfer to faucet done`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
