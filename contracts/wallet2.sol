217e538788b4a45ca4e6d4d06c8a615fcc6f2410
shiyu <zhangshiyu@hashkey.com>
2024-05-15 18:32:10 +0800

    update .sol

--- contracts/wallet2.sol
+++ contracts/wallet2.sol
@@ -4,12 +4,15 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Wallet {
+contract Wallet2 {
 
     constructor(){}
 
-    function transferETH(address payable recipient, uint256 amount) external payable {
-        recipient.transfer(amount);
+    function transferERC20(address token1, address token2, address payable recipient, uint256 amount) external payable {
+        IERC20(token1).transfer(recipient, amount);
+        IERC20(token1).transfer(recipient, amount);
+        IERC20(token2).transfer(recipient, amount);
+        IERC20(token2).transfer(recipient, amount);
 }
     receive() external payable { }
 }
--- contracts/walleteth.sol
+++ contracts/walleteth.sol
@@ -4,12 +4,11 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract WalletERC20 {
+contract WalletEth {
 
     constructor(){}
 
-    function transferERC20(address token, address payable recipient, uint256 amount) external payable {
-        IERC20(token).transfer(recipient, amount);
+    function transferETH(address payable recipient, uint256 amount) external payable {
         recipient.transfer(amount);
 }
     receive() external payable { }
